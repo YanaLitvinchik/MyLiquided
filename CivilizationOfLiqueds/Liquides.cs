332747913dfa5f5a21be2441ae1f98e52de1378f
Anton <silversilk@ukr.net>
2018-04-05 16:50:55 +0300

    Add project files.

---
+++ CivilizationOfLiqueds/Liquides.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivilizationOfLiquides
+{
+    class Liquides
+    {
+        public long Population { get; set; }
+        public long TimeChanging { get; set; }
+        public IState CurrentState { get; set; }
+
+    }
+}
---
+++ CivilizationOfLiqueds/State.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//c.CurrentState = new ContinueWar();
+namespace CivilizationOfLiquides
+{    
+    class TurnToSun : IState
+    {
+        public void ChangeState(Liquides l)
+        {
+            Random r = new Random();
+            l.Population -= (long)(l.Population * r.Next(20, 30));
+            //l.CurrentState = new State();
+        }
+    }
+    class Continue : IState
+    {
+        public void ChangeState(Liquides l)
+        {
+            l.Population -= (long)(l.Population * 5);//5% die
+
+        }
+    }
+}
